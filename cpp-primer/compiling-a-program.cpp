e33e601a0d68a516b060c6bfcb564a708a93b65e
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-09-18 22:33:22 -0700

    Created a basic program to test whether my computer can compile and run C++ programs.

---
+++ cpp-primer/compiling-a-program.cpp
@@ -0,0 +1,8 @@
+// To compile a program using g++ in VS Code, use the following command format
+// g++ -o program-name file-name.cpp
+
+// To run a program called program-name.exe, use ./program-name
+
+int main() {
+  return 0;
+}
