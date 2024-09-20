65604555ae95180ed908e712531b7274ba1b968d
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-09-20 13:21:11 -0700

    A little practice using comments.

---
+++ cpp-primer/1.3_comments.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// single line comment
+/* paired comment */
+
+/*
+ * multi-line comment
+ * hi
+ */
+
+// /*
+//  * use single line comments for large blocks of code
+//  */
+// std::cout << "lol" << std::endl;
+
+int main() {
+  std::cout << "/*";
+  std::cout << "*/";
+
+  // pay close attention to placement of "" !
+  std::cout << /* "*/" /* "/*" */;
+  
+  return 0;
+}
