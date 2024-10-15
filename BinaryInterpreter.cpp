5853f3cba95d7dbf7a83410cf162af8443d9cabc
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-10-15 16:19:02 -0700

    Implemented a function that translates a binary number (e.g., 11001) into its decimal value (e.g., 25). Realistically speaking, this code isn't very useful application-wise (since better libraries for number systems already exist). However, it was a fun exercise writing it. (:

--- BinaryInterpreter.cpp
+++ BinaryInterpreter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <string>
+#include <string>  // string methods (e.g., to_string)
+#include <cmath>  // exponents
 
 // Recursively divide a decimal number by 2 and use the remainders
 // to calculate its binary form
@@ -12,15 +13,53 @@ std::string printDecimalAsBinary(int decNum) {
   return printDecimalAsBinary(decNum / 2) + std::to_string(decNum % 2);
 }
 
+// int countDigits(int num) {
+//   int totalDigits = 0;
+//   while (num > 0) {
+//     num /= 10;
+//     ++totalDigits;
+//   }
+//   return totalDigits;
+// }
+
+
+
+// 11001
+int printBinaryAsDecimal(int binaryNum) {
+  int decimalSum = 0;
+  int power = 0;  // the exponent each digit represents (e.g. 2^0, 2^1, 2^2)
+
+  while (binaryNum > 0) {
+    int digitWeight = pow(2.0, power) * (binaryNum % 10);
+    decimalSum += digitWeight;
+
+    binaryNum /= 10;
+    ++power;
+  }
+
+  return decimalSum;
+}
+
 int main() {
-  int decimalNumber = 0;
-  std::cout << "Enter a number in Decimal: ";
-  if (!(std::cin >> decimalNumber)) {
+  // int decimalNumber = 0;
+  // std::cout << "Enter a number in Decimal: ";
+  // if (!(std::cin >> decimalNumber)) {
+  //   std::cout << "Invalid input. Exiting." << std::endl;
+  //   return -1;
+  // }
+
+  // std::cout << decimalNumber << " in Binary is: "
+  //           << printDecimalAsBinary(decimalNumber) << std::endl;
+
+  int binaryNumber = 0;
+  std::cout << "Enter a number in Binary: ";
+  if (!(std::cin >> binaryNumber)) {
     std::cout << "Invalid input. Exiting." << std::endl;
     return -1;
   }
 
-  std::cout << decimalNumber << " in Binary is: "
-            << printDecimalAsBinary(decimalNumber) << std::endl;
+  std::cout << binaryNumber << " in Decimal is: "
+            << printBinaryAsDecimal(binaryNumber) << std::endl;
+
   return 0;
 }
