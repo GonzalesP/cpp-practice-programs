c6058e540cfea6a4499d9bddb59e86385a2d0326
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-10-16 21:18:16 -0700

    Solved it! Although, I'm pretty sure there is much room for optimization (e.g., too many nested if statements...).

--- 1405-longest-happy-string.cpp
+++ 1405-longest-happy-string.cpp
@@ -3,105 +3,30 @@
 
 class Solution {
 public:
-    std::string greatestStartingLetter(int a, int b, int c) {
+    int startingLetter(int a, int b, int c) {
         if (a >= b && a >= c) {
-            return "a";
+            return 0;
         }
         else if (b >= a && b >= c) {
-            return "b";
-        }
-        return "c";
-    }
-
-    std::string nextSubstring(int a, int b, int c, std::string nextLetter, int numLetter) {
-        std::string nextSub = "";
-        if (numLetter == 2) {
-            nextSub = nextLetter + nextLetter;
+            return 1;
         }
         else {
-            nextSub = nextLetter;
-        }
-
-        if (nextLetter == "a") {
-            a -= numLetter;
-            if (b == 0 && c == 0) {
-                return nextSub;
-            }
-            else if (b >= c) {
-                if (b >= a) {
-                    return nextSub + nextSubstring(a, b, c, "b", 2);
-                }
-                else {
-                    return nextSub + nextSubstring(a, b, c, "b", 1);
-                }
-            }
-            else {
-                if (c >= a) {
-                    return nextSub + nextSubstring(a, b, c, "c", 2);
-                }
-                else {
-                    return nextSub + nextSubstring(a, b, c, "c", 1);
-                }
-            }
-        }
-        else if (nextLetter == "b") {
-            b -= numLetter;
-            if (a == 0 && c == 0) {
-                return nextSub;
-            }
-            else if (a >= c) {
-                if (a >= b) {
-                    return nextSub + nextSubstring(a, b, c, "a", 2);
-                }
-                else {
-                    return nextSub + nextSubstring(a, b, c, "a", 1);
-                }
-            }
-            else {
-                if (c >= b) {
-                    return nextSub + nextSubstring(a, b, c, "c", 2);
-                }
-                else {
-                    return nextSub + nextSubstring(a, b, c, "c", 1);
-                }
-            }
-        }
-        else {
-            c -= numLetter;
-            if (a == 0 && b == 0) {
-                return nextSub;
-            }
-            else if (a >= b) {
-                if (a >= c) {
-                    return nextSub + nextSubstring(a, b, c, "a", 2);
-                }
-                else {
-                    return nextSub + nextSubstring(a, b, c, "a", 1);
-                }
-            }
-            else {
-                if (b >= c) {
-                    return nextSub + nextSubstring(a, b, c, "b", 2);
-                }
-                else {
-                    return nextSub + nextSubstring(a, b, c, "b", 1);
-                }
-            }
+            return 2;
         }
     }
 
     std::string longestDiverseString(int a, int b, int c) {
         std::string output = "";
-        std::string nextLetter = greatestStartingLetter(a, b, c);
+        int nextLetter = startingLetter(a, b, c);
         int numLetter = 0;
 
-        if (nextLetter == "a") {
+        if (nextLetter == 0) {
             if (a >= 2)
                 numLetter = 2;
             else
                 numLetter = 1;
         }
-        else if (nextLetter == "b") {
+        else if (nextLetter == 1) {
             if (b >= 2)
                 numLetter = 2;
             else
@@ -114,13 +39,115 @@ public:
                 numLetter = 1;
         }
 
-        output += nextSubstring(a, b, c, nextLetter, numLetter);
+        bool moreOptions = true;
+
+        while(moreOptions) {
+            if (nextLetter == 0) {
+                if (numLetter == 2) {
+                    output += "aa";
+                }
+                else {
+                    output += "a";
+                }
+                a -= numLetter;
+
+                if (b == 0 && c == 0) {
+                    moreOptions = false;
+                }
+                else if (b >= c) {
+                    nextLetter = 1;
+                    if (b >= a && b >= 2) {
+                        numLetter = 2;
+                    }
+                    else {
+                        numLetter = 1;
+                    }
+                }
+                else {
+                    nextLetter = 2;
+                    if (c >= a && c >= 2) {
+                        numLetter = 2;
+                    }
+                    else {
+                        numLetter = 1;
+                    }
+                }
+            }
+            else if (nextLetter == 1) {
+                if (numLetter == 2) {
+                    output += "bb";
+                }
+                else {
+                    output += "b";
+                }
+                b -= numLetter;
+
+                if (a == 0 && c == 0) {
+                    moreOptions = false;
+                }
+                else if (a >= c) {
+                    nextLetter = 0;
+                    if (a >= b && a >= 2) {
+                        numLetter = 2;
+                    }
+                    else {
+                        numLetter = 1;
+                    }
+                }
+                else {
+                    nextLetter = 2;
+                    if (c >= b && c >= 2) {
+                        numLetter = 2;
+                    }
+                    else {
+                        numLetter = 1;
+                    }
+                }
+            }
+            else {
+                if (numLetter == 2) {
+                    output += "cc";
+                }
+                else {
+                    output += "c";
+                }
+                c -= numLetter;
+
+                if (a == 0 && b == 0) {
+                    moreOptions = false;
+                }
+                else if (a >= b) {
+                    nextLetter = 0;
+                    if (a >= c && a >= 2) {
+                        numLetter = 2;
+                    }
+                    else {
+                        numLetter = 1;
+                    }
+                }
+                else {
+                    nextLetter = 1;
+                    if (b >= c && b >= 2) {
+                        numLetter = 2;
+                    }
+                    else {
+                        numLetter = 1;
+                    }
+                }
+            }
+        }
+
         return output;
     }
 };
 
 int main() {
+  int a = 0, b = 0, c = 0;
+  std::cout << "Choose 3 numbers for a, b, c" << std::endl;
+  std::cout << "0 <= a, b, c <= 100 AND a + b + c > 0" << std::endl;
+  std::cin >> a >> b >> c;
+
   Solution s;
-  std::cout << s.longestDiverseString(1, 1, 1) << std::endl;
+  std::cout << s.longestDiverseString(a, b, c) << std::endl;
   return 0;
 }
