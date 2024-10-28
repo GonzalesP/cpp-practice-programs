36a3462507fd4c7fe14f121ad62654f4800f01d5
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-10-28 15:55:24 -0700

    Solved leetcode #13. I'm not sure if the if-statements/iteration is optimal, but the solution works!

--- leetcode/13-Roman-to-Integer.cpp
+++ leetcode/13-Roman-to-Integer.cpp
@@ -5,10 +5,7 @@ class Solution {
 public:
     int romanToInt(std::string s) {
         // character weight, count
-        std::unordered_map<int, int> digits = {{1, 0}, {4, 0}, {5, 0}, {9, 0},
-                                          {10, 0}, {40, 0}, {50, 0}, {90, 0},
-                                          {100, 0}, {400, 0}, {500, 0},
-                                          {900, 0}, {1000, 0}};
+        std::unordered_map<int, int> digits = {{1, 0}, {4, 0}, {5, 0}, {9, 0}};
 
         for (int i = 0; i < s.length(); i++) {
             char c = s[i];
@@ -33,59 +30,67 @@ public:
             else if (c == 'X') {
                 if (i < s.length() - 1) {
                     if (s[i+1] == 'L') {
-                        ++digits[40];
+                        digits[4] += 10;
                         ++i;
                     }
                     else if (s[i+1] == 'C') {
-                        ++digits[90];
+                        digits[9] += 10;
                         ++i;
                     }
                     else {
-                        ++digits[10];
+                        digits[1] += 10;
                     }
                 }
                 else {
-                    ++digits[10];
+                    digits[1] += 10;
                 }
             }
             else if (c == 'C') {
                 if (i < s.length() - 1) {
                     if (s[i+1] == 'D') {
-                        ++digits[400];
+                        digits[4] += 100;
                         ++i;
                     }
                     else if (s[i+1] == 'M') {
-                        ++digits[900];
+                        digits[9] += 100;
                         ++i;
                     }
                     else {
-                        ++digits[100];
+                        digits[1] += 100;
                     }
                 }
                 else {
-                    ++digits[100];
+                    digits[1] += 100;
                 }
             }
             else if (c == 'V') {
                 ++digits[5];
             }
             else if (c == 'L') {
-                ++digits[50];
+                digits[5] += 10;
             }
             else if (c == 'D') {
-                ++digits[500];
+                digits[5] += 100;
             }
             else if (c == 'M') {
-                ++digits[1000];
+                digits[1] += 1000;
             }
         }
 
-        return digits[1] + 4 * digits[4] + 5 * digits[5] + 9 * digits[9] +
-               10 * digits[10] + 40 * digits[40] + 50 * digits[50] +
-               90 * digits[90] + 100 * digits[100] + 400 * digits[400] +
-               500 * digits[500] + 900 * digits[900] + 1000 * digits[1000];
+        return digits[1] + 4 * digits[4] + 5 * digits[5] + 9 * digits[9];
     }
 };
+
+int main() {
+    Solution s;
+    std::string romanNumeral;
+    std::cout << "Enter a roman numeral: " << std::endl;
+    std::cin >> romanNumeral;
+    std::cout << "The roman numeral " << romanNumeral << " is equal to: "
+              << s.romanToInt(romanNumeral) << std::endl;
+
+    return 0;
+}
 
 // better alternative: use {1, 0}, {4, 0}, {5, 0}, and {9, 0}
 // and add 1, 10, 100, or 1000 depending on symbol (save more space)
