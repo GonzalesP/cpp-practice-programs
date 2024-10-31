34ee4e42ac961e5ce345f775eb7b0265deabf7d3
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-10-31 11:33:56 -0700

    Solved leetcode #14, 'Longest Common Prefix.'

---
+++ leetcode/14-Longest-Common-Prefix.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::string longestCommonPrefix(std::vector<std::string>& strs) {
+        int readerIndex = 0;  // nth char of all strings in strs
+        int prefixLength = 0;  // length of common prefix
+        char r;  // character at readerIndex
+        bool prefixEnd = false;
+
+        while(!prefixEnd) {
+            if (readerIndex >= strs.at(0).length()) {
+                break;
+            }
+
+            r = strs.at(0)[readerIndex];
+            for (int i = 1; i < strs.size(); ++i) {
+                if (readerIndex >= strs.at(i).length()) {
+                    prefixEnd = true;
+                    break;
+                }
+                else if (strs.at(i)[readerIndex] != r) {
+                    prefixEnd = true;
+                    break;
+                }
+            }
+
+            if (!prefixEnd) {
+                ++prefixLength;
+            }
+            ++readerIndex;
+        }
+
+        return strs.at(0).substr(0, prefixLength);
+    }
+};
