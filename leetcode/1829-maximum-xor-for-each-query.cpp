61e4da6bf3d80ccd59c0c1f7d5815a20df3f628a
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-11-08 02:27:25 -0800

    Finished adding the optimization described previously.

--- leetcode/1829-maximum-xor-for-each-query.cpp
+++ leetcode/1829-maximum-xor-for-each-query.cpp
@@ -5,13 +5,13 @@ public:
     std::vector<int> getMaximumXor(std::vector<int>& nums, int maximumBit) {
         std::vector<int> answer;
         int maxXOR = (int) pow(2, maximumBit) - 1;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int nthXOR = nums.at(0);
-            for (int j = 1; j < nums.size() - i; ++j) {
-                nthXOR = nthXOR ^ nums.at(j);
-            }
-            answer.push_back(nthXOR ^ maxXOR);
+
+        int nthXOR = nums.at(0);
+        answer.insert(answer.begin(), nthXOR ^ maxXOR);
+
+        for (int i = 1; i < nums.size(); ++i) {
+            nthXOR = nthXOR ^ nums.at(i);
+            answer.insert(answer.begin(), nthXOR ^ maxXOR);
         }
         return answer;
     }
