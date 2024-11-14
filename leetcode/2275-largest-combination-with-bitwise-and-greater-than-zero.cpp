47cf480d48cbc34cab75dc0e83d0c8049da63f75
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-11-14 14:47:01 -0800

    leetcode #2275

--- leetcode/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ leetcode/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,32 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 class Solution {
 public:
     int largestCombination(std::vector<int>& candidates) {
-        int largestComboSize = 0;
-        for (int i = 1; i < pow(2, candidates.size()); ++i) {
-            // select combo of candidates
-            int bitwiseMap = i;
-            std::vector<int> comboI;
-            for (int candidate = 0; candidate < candidates.size(); ++candidate) {
-                if (bitwiseMap % 2 == 1) {
-                    comboI.push_back(candidates.at(candidate));
+        // bit position, # of candidates
+        std::unordered_map<int, int> sharedBits;
+        int largest_combo = 0;
+
+        for (int can = 0; can < candidates.size(); ++can) {
+            int bit = 1;
+            int candidate = candidates.at(can);
+            while (candidate > 0) {
+                // bit is set to 1, increase count
+                if (candidate & 1) {
+                    // bit already exists
+                    // C++ 20 - if (sharedBits.contains(bits))
+                    if (sharedBits.find(bit) != sharedBits.end()) {
+                        ++sharedBits[bit];
+                    }
+                    // bit doesn't exist yet
+                    else {
+                        sharedBits[bit] = 1;
+                    }
+
+                    // check if combo record should be updated
+                    if (sharedBits[bit] > largest_combo) {
+                        largest_combo = sharedBits[bit];
+                    }
                 }
-                bitwiseMap = bitwiseMap >> 1;
-            }
-
-            // calculate bitwise AND of combo I
-            int comboAND = comboI.at(0);
-            for (int cand = 1; cand < comboI.size(); ++cand) {
-                comboAND = comboAND & comboI.at(cand);
-            }
-
-            if (comboAND > 0 && comboI.size() > largestComboSize) {
-                largestComboSize = comboI.size();
+                ++bit;
+                candidate = candidate >> 1;
             }
         }
 
-        return largestComboSize;
+        return largest_combo;
     }
 };
+
+int main() {
+    int candidate;
+    std::vector<int> candidates;
+    Solution s;
+
+    std::cout << "Enter a list of candidates to find the largest combo with "
+              << "bitwise AND > 0: " << std::endl;
+    
+    while (std::cin >> candidate) {
+        candidates.push_back(candidate);
+    }
+
+    std::cout << "The largest combo of bitwise AND > 0 consists of THIS many "
+              << "candidates:" << std::endl;
+    std::cout << s.largestCombination(candidates) << std::endl;
+
+    return 0;
+}
