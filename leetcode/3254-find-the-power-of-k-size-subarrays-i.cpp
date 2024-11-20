d6bdd5bfd99b6898fc81be57894e62aa289abece
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-11-19 23:57:27 -0800

    Added comments to leetcode #3254

--- leetcode/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ leetcode/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -7,6 +7,7 @@ public:
         std::unordered_map<int, int> powerless_indices;
         std::vector<int> powers;
 
+        // record indices that are not consecutive (invalid)
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] != nums[i - 1] + 1) {
                 powerless_indices[i] = i;
@@ -15,6 +16,8 @@ public:
 
         for (int i = 0; i < nums.size() - k + 1; ++i) {
             bool powerless = false;
+            // for each element in subarray (except first), find out if duplicates exist
+            // using hash table lookup. if so, the subarray is invalid (powerless)
             for (int j = i + 1; j < i + k; ++j) {
                 if (powerless_indices.find(j) != powerless_indices.end()) {
                     powerless = true;
@@ -24,6 +27,7 @@ public:
             if (powerless) {
                 powers.push_back(-1);
             }
+            // otherwise, if there are no duplicates, add the power of the subarray
             else {
                 powers.push_back(nums[i + k - 1]);
             }
