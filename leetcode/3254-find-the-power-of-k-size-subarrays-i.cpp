0906859f8bafeb5668d168896a6dc7f9a9f9cbe7
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-11-19 23:50:12 -0800

    leetcode #3254 - could improve time and memory

---
+++ leetcode/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> resultsArray(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> powerless_indices;
+        std::vector<int> powers;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i - 1] + 1) {
+                powerless_indices[i] = i;
+            }
+        }
+
+        for (int i = 0; i < nums.size() - k + 1; ++i) {
+            bool powerless = false;
+            for (int j = i + 1; j < i + k; ++j) {
+                if (powerless_indices.find(j) != powerless_indices.end()) {
+                    powerless = true;
+                    break;
+                }
+            }
+            if (powerless) {
+                powers.push_back(-1);
+            }
+            else {
+                powers.push_back(nums[i + k - 1]);
+            }
+        }
+
+        return powers;
+    }
+};
