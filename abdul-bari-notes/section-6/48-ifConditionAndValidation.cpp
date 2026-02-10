9a91472bffbee232a31d9238d48e81f191a9213c
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-02-10 00:31:07 -0800

    Practice with conditional statements

--- abdul-bari-notes/section-6/48-ifConditionAndValidation.cpp
+++ abdul-bari-notes/section-6/48-ifConditionAndValidation.cpp
@@ -12,5 +12,29 @@ int main() {
         c = a / b;
         std::cout << "The quotient is " << c << std::endl;
     }
+    std::cout << std::endl;
+
+    // 50 - Compound Conditional Statement
+    int hour;  // from 0 to 23
+    std::cout << "Enter an hour: " << std::endl;
+    std::cin >> hour;
+    if (hour >= 9 && hour <= 17) {  // assume work is from 9-5
+        std::cout << "This is a working hour" << std::endl;
+    }
+    else {
+        std::cout << "This is a leisure hour" << std::endl;
+    }
+
+    // 51 - Practice Program using Compound Conditions
+    // "suppose children (12 and under) and seniors (50+) get discounts"
+    // "check if the age is eligible or not"
+    int age;
+    std::cout << "Enter an age: " << std::endl;
+    std::cin >> age;
+    if (age <= 12 || age >= 50)
+        std::cout << "You are eligible for discounts" << std::endl;
+    else   
+        std::cout << "You are ineligible for discounts" << std::endl;
+    
     return 0;
 }
