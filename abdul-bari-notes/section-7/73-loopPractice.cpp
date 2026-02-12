ee1aba417be82cc4ba86b53a5561b6fc93920f58
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-02-11 19:45:15 -0800

    file 73 has more loop practice added. also created another file for more loops involving reversed numbers

--- abdul-bari-notes/section-7/73-loopPractice.cpp
+++ abdul-bari-notes/section-7/73-loopPractice.cpp
@@ -5,26 +5,26 @@ int main() {
     std::cout << "Enter n: ";
     std::cin >> n;
 
-    // generate multiplication with first 10 terms of n
+    // 73: generate multiplication with first 10 terms of n
     for (i = 1; i <= 10; ++i) {
         std::cout << n << " x " << i << " = " << n * i << std::endl;
     }
 
-    // summation of first N natural numbers
+    // 74: summation of first N natural numbers
     int j, sum = 0;
     for (j = 1; j <= n; ++j) {
         sum += j;
     }
     std::cout << "Sum of first " << n << " numbers = " << sum << std::endl;
 
-    // factorial n!
+    // 75: factorial n!
     int k, prod = 1;
     for (k = 1; k <= n; ++k) {
         prod *= k;
     }
     std::cout << n << "! = " << prod << std::endl;
 
-    // factors of n
+    // 76: factors of n
     int f;
     std::cout << "Factors of n: ";
     for (f = 1; f <= n; ++f) {
@@ -33,7 +33,7 @@ int main() {
     }
     std::cout << std::endl;
 
-    // perfect number check
+    // 77: perfect number check
     int p;
     int sumFactor = 0;
     for (p = 1; p <= n; ++p) {
@@ -45,6 +45,24 @@ int main() {
     }
     else {
         std::cout << n << " is NOT a perfect number." << std::endl;
+    }
+
+    // 78: checking if a number is prime
+    // primes only have two factors: itself and 1
+    int q;
+    bool prime = true;
+    for (q = 2; q < n; ++q) {
+        if (n % q == 0) {
+            prime = false;
+            break;
+        }
+    }
+
+    if (prime) {
+        std::cout << n << " is prime." << std::endl;
+    }
+    else {
+        std::cout << n << " is not prime." << std::endl;
     }
 
     return 0;
