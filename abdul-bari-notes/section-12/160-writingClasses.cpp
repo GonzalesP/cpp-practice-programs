0c3be71b75e7b2d1a47bebc6c4f8099d84585de5
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-02-17 22:00:44 -0800

    Learning about classes.

---
+++ abdul-bari-notes/section-12/160-writingClasses.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Rectangle
+{
+public:
+    int length;
+    int breadth;
+
+    int area() {
+        return length * breadth;
+    }
+    int perimeter() {
+        return 2 * (length + breadth);
+    }
+};
+
+int main() {
+    Rectangle r1;
+    r1.length = 10;
+    r1.breadth = 5;
+    std::cout << "Area of r1 is: " << r1.area() << std::endl;
+    std::cout << "Perimeter of r1 is: " << r1.perimeter() << std::endl;
+
+    return 0;
+}
