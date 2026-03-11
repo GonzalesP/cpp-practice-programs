5e958f6a3c7ee073631c5c29902a9d4c19254b3d
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-03-11 12:14:11 -0700

    more tests with shared ptrs

--- abdul-bari-notes/section-extra/chess/shared_ptrTesting.cpp
+++ abdul-bari-notes/section-extra/chess/shared_ptrTesting.cpp
@@ -66,7 +66,7 @@ int main() {
     const int files = 8;  // file A-H
 
     std::vector<std::vector<std::shared_ptr<Piece>>> chessBoard(
-        ranks, std::vector<std::shared_ptr<Piece>> (files)
+        ranks, std::vector<std::shared_ptr<Piece>> (files, nullptr)
     );
 
     // a8 - h8
@@ -79,10 +79,46 @@ int main() {
     chessBoard[0][6] = std::make_shared<Knight>();
     chessBoard[0][7] = std::make_shared<Rook>();
 
+    std::vector<std::shared_ptr<Piece>> playerPieces;
+    playerPieces.push_back(chessBoard[0][0]);
+    playerPieces.push_back(chessBoard[0][1]);
+    playerPieces.push_back(chessBoard[0][2]);
+    playerPieces.push_back(chessBoard[0][3]);
+    playerPieces.push_back(chessBoard[0][4]);
+    playerPieces.push_back(chessBoard[0][5]);
+    playerPieces.push_back(chessBoard[0][6]);
+    playerPieces.push_back(chessBoard[0][7]);
+
+    std::cout << std::endl << "chessBoard" << std::endl;
     for (std::vector<std::shared_ptr<Piece>>& rank : chessBoard) {
-        for (std::shared_ptr<Piece>& p : rank)
-            p->print();
+        for (std::shared_ptr<Piece>& ptr : rank) {
+            if (ptr != nullptr) {
+                ptr->print();
+                std::cout << "ptr: " << ptr << std::endl;
+                std::cout << "&ptr: " << &ptr << std::endl;
+            }
+        }
+    }
+
+    std::cout << std::endl << "playerPieces" << std::endl;
+    for (std::shared_ptr<Piece>& ptr : playerPieces) {
+        ptr->print();
+        std::cout << "ptr: " << ptr << std::endl;
+        std::cout << "&ptr: " << &ptr << std::endl;
     }
 
     return 0;
 }
+
+// cannot use unique_ptr if you're going to store Piece in two containers
+// one reference to a Piece may exist in chessBoard (2D Vector)
+    // (e.g., chessBoard[0][0] points to the Rook on a8)
+// and another reference to the same Piece may exist
+// in a player's pieces (1D Vector)
+    // (e.g., playerPieces[0] points to the Rook on a8, same as chessBoard)
+
+// so, the trade off is that we don't use unique_ptr in order to store
+// multiple references to the same object. and the benefit of having a separate
+// container for playerPieces is that when calculating a player's moves,
+// it is easier to inspect their pieces independently (n pieces) rather
+// than inspecting the entire board (n^2 pieces)
