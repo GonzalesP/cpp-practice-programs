244cea8932a270331b5c26eebe4c469b98804ac6
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-03-24 20:37:31 -0700

    structure as parameter

---
+++ abdul-bari-notes/dsa/section-2/20-structure-as-parameter.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+struct Rectangle {
+    int length;
+    int breadth;
+};
+
+// pass by value
+int area(struct Rectangle r1) {
+    return r1.length * r1.breadth;
+}
+
+// pass by reference
+void changeLength(struct Rectangle& r1, int l) {
+    r1.length = l;
+}
+
+// pass by address
+void changeBreadth(struct Rectangle* r1, int b) {
+    r1->breadth = b;
+}
+
+
+struct Test {
+    int A[5];
+    int n;
+};
+
+// although you cannot pass an array by value,
+// a struct that HAS an array CAN be passed by value (the array will be copied)
+void fun(struct Test t1) {
+    t1.A[0] = 10;  // this will not change the actual parameter's array
+    t1.A[1] = 9;
+    t1.A[2] = 8;
+    t1.A[3] = 7;
+    t1.A[4] = 6;
+}
+
+int main() {
+    struct Rectangle r = {10, 5};
+    std::cout << "area(r): " << area(r) << std::endl;
+
+    changeLength(r, 8);
+    changeBreadth(&r, 8);
+
+    std::cout << "area(r) after update: " << area(r) << std::endl;
+
+
+    struct Test t = {{2, 4, 6, 8, 10}, 5};
+    fun(t);
+
+    for (int x : t.A)
+        std::cout << x << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
