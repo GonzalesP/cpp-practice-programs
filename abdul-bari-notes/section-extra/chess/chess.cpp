b31f9c06b35f0897a98f21ca54b1e39e6e5a9c59
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-07-31 23:20:09 -0700

    comments

--- abdul-bari-notes/section-extra/chess/chess.cpp
+++ abdul-bari-notes/section-extra/chess/chess.cpp
@@ -390,7 +390,7 @@ bool operator==(square s1, square s2) {  // used to compare squares (same coordi
 
 moveInput stringToMoveInput(std::string input) {  // used to convert user input (string form) into a moveInput data structure
     // check for invalid strings
-    if (input.size() != 4)  // each move needs 4 characters: 2 for the starting square, 2 for the ending square
+    if (input.size() != 4)  // each move needs 4 characters: 2 for the starting square, 2 for the destination square
         throw 1;  // throw an exception if there are not exactly four characters
     else if (input[3] - '0' < 1 || input[3] - '0' > 8)  // destination square's rank needs to be 1-8
         throw 1;  // throw an exception if the destination square's rank is invalid
@@ -399,7 +399,7 @@ moveInput stringToMoveInput(std::string input) {  // used to convert user input 
     else if (input[2] < 65 || (input[2] > 72 && input[2] < 97) || input[2] > 104)  // file needs to be A-H or a-h
         throw 1;  // throw an exception if the destination square's file is invalid
     else if (input[0] < 65 || (input[0] > 72 && input[0] < 97) || input[0] > 104)  // file needs to be A-H or a-h
-        throw 1;
+        throw 1;  // throw an exception if the starting square's file is invalid
     
     square start;
     square end;
