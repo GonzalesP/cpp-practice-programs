21e8ec3d2ed6cfb70aee769bb3916f09260e9053
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-08-12 23:48:38 -0700

    comments

--- abdul-bari-notes/section-extra/chess/chess.cpp
+++ abdul-bari-notes/section-extra/chess/chess.cpp
@@ -424,7 +424,7 @@ moveInput stringToMoveInput(std::string input) {  // used to convert user input 
 
 // Piece methods
 Piece::Piece(pieceType type, playerColor color, square s) {  // constructor for piece objects
-    this->type = type;
+    this->type = type;  // take in type parameter
     this->color = color;
     this->position = s;
 }
