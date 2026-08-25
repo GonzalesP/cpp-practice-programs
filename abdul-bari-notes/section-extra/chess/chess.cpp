7a46d6d898720554c3642fb75ec6d5ed9ea2e50d
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-08-24 23:30:15 -0700

    comments

--- abdul-bari-notes/section-extra/chess/chess.cpp
+++ abdul-bari-notes/section-extra/chess/chess.cpp
@@ -445,7 +445,7 @@ std::ostream& operator<<(std::ostream& os, PiecePtr p) {  // print a piece
 Pawn::Pawn(pieceType type, playerColor color, square s) : Piece(type, color, s) {}  // constructor for Pawn objcts (inherts Piece)
 Pawn::Pawn(pieceType type, playerColor color, row row, col col) : Pawn(type, color, {row, col}) {}  // second constructor for Pawns (non-square input)
 
-void Pawn::print(std::ostream& os) {
+void Pawn::print(std::ostream& os) {  // method to print Pawn (white is uppercase, black is lowercase)
     switch(color) {
         case white:
             os << "P";
