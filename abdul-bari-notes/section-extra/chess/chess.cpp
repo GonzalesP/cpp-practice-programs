1faeb70f7130f71e317fd510e877341657a61969
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-09-17 21:40:53 -0700

    comments

--- abdul-bari-notes/section-extra/chess/chess.cpp
+++ abdul-bari-notes/section-extra/chess/chess.cpp
@@ -472,7 +472,7 @@ void Knight::print(std::ostream& os) {  // method to print Knight (white upperca
 }  // end of Knight print method
 
 Bishop::Bishop(pieceType type, playerColor color, square s) : Piece(type, color, s) {}  // constructor for Bishop objects (inherits Piece)
-Bishop::Bishop(pieceType type, playerColor color, row row, col col) : Bishop(type, color, {row, col}) {}
+Bishop::Bishop(pieceType type, playerColor color, row row, col col) : Bishop(type, color, {row, col}) {}  // second constructor for Bishops
 
 void Bishop::print(std::ostream& os) {
     switch(color) {
