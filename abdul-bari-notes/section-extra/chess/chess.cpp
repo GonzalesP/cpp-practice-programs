1bbc63c17d800c0eb7fdaf24bacafea12dd5a982
GonzalesP <GonzalesP@csu.fullerton.edu>
2026-09-20 23:32:11 -0700

    comments

--- abdul-bari-notes/section-extra/chess/chess.cpp
+++ abdul-bari-notes/section-extra/chess/chess.cpp
@@ -476,7 +476,7 @@ Bishop::Bishop(pieceType type, playerColor color, row row, col col) : Bishop(typ
 
 void Bishop::print(std::ostream& os) {  // method to print Bishop (white uppercase, black lowercase)
     switch(color) {  // determine whether to print in caps or lowercase
-        case white:
+        case white:  // if the piece is white, it will be a capital B
             os << "B";
             break;
         case black:
